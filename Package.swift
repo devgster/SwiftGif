d57a930a7ca57a77a7df188b5e63906df0211803
박경우 <devgster@gmail.com>
2024-12-18 11:10:42 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,23 +1,33 @@
-// swift-tools-version:6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "SwiftGif",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v10), // iOS 10.0 이상 지원
+        .macOS(.v10_15), // macOS 10.15 이상 지원
+        .tvOS(.v10) // tvOS 10.0 이상 지원
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftGif",
-            targets: ["SwiftGif"]),
+            targets: ["SwiftGif"]
+        )
     ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftGif",
-            path: "SwiftGif"),
+            path: "SwiftGifCommon",
+            exclude: ["README.md"], // 필요시 제외할 파일 설정
+            resources: [
+                .process("Resources") // 이미지, 데이터 파일 등 리소스 처리
+            ]
+        ),
+        .testTarget(
+            name: "SwiftGifTests",
+            dependencies: ["SwiftGif"],
+            path: "SwiftGifTests"
+        )
     ]
 )
